3eb983b8fb2e6a3ee588500f1bdb353edf316c70
MarjoryMatos <marjory_matos@hotmail.com>
2022-06-07 18:46:47 -0300

    atualizando crud metodo put

--- src/app/edit/tema-edit/tema-edit.component.ts
+++ src/app/edit/tema-edit/tema-edit.component.ts
@@ -11,12 +11,35 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaEditComponent implements OnInit {
   
+  tema: Tema= new Tema()
+
   constructor(
+    private temaService: TemaService,
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    let id = this.route.snapshot.params['id']
+    this.findByIdTema(id)
   }
 
-  
-  
+findByIdTema(id: number){
+  this.temaService.getByIdTema(id).subscribe((resp: Tema)=>{
+    this.tema = resp
+  })
+
+}  
+
+atualizar(){
+  this.temaService.putTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert("Tema atualizado com sucesso!")
+      this.router.navigate(['/tema'])
+  })
+}
 }
